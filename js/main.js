024aed6776c0656c07311d1aab4111e23f817ca3
JohnathanRB219 <johnathanrbaldwin@gmail.com>
2018-06-16 16:56:33 -0700

    Week7Ex

---
+++ js/main.js
@@ -0,0 +1,27 @@
+document.getElementById("unique").innerText = "New Message";
+
+
+
+
+function getFunky() {
+	
+	var how = 
+	document.getElementById("boom").value
+	console.log(how)
+
+	document.getElementById("code").innerText = how
+}
+
+var myArray = ["when","how","why","what","who"];
+// for (i=0; i < myArray.length; i++)
+	console.log(myArray[0]);
+	console.log(myArray[1]);
+	console.log(myArray[2]);
+	console.log(myArray[3]);
+	console.log(myArray[4]);
+	
+
+var student ={Name:"Bob", age:"69", enrolled:"true"}
+console.log(student.Name)
+console.log(student.age)
+console.log(student.enrolled)
